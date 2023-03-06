9a2bed1f934c4c8c2ed4c10a4fb2ec29fe8f6dfe
cappeloow <cappe1996@hotmail.com>
2023-03-06 22:27:55 +0100

    some small changes, removed a folder with some files that wasn't used for this specific API

--- src/diary/diary.controller.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response } from "express-serve-static-core";
-
--- src/diary/diary.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-// import getAllPosts from "./diary.controller";
-const route = express.Router();
-
-//isAdmin function will determine if we can get all or just by the userID.
-route.get('/library', );
-
-module.exports = route;
